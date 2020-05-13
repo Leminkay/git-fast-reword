f46f72fa8a5192a5c9507510ad7bb5855ebe45d7
Leminkay <Leminkay@gmail.com>
2020-05-13 21:53:23 +0300

    working

--- reword.py
+++ reword.py
@@ -1,13 +1,16 @@
 import os
-import sys
 import hashlib
 import zlib
+import sys
 
 
-homeDir = "/home/kal/jet/.git/"
+#homeDir = "/home/kal/jet/.git/"
+homeDir = os.getcwd() + "/.git/"
+
 commDict = {}
 
 
+# parsing logs with this function
 def parse_line(inString):
     line = inString.replace("\t", ' ').replace(" (initial)", '(initial)')
     line = line.split(' ')
@@ -21,6 +24,8 @@ def parse_line(inString):
         line[6] = "commit (initial):"
     return line
 
+
+# parsed list to string
 def parsed_to_string(line):
     str = ''
     for i in range(5):
@@ -29,19 +34,20 @@ def parsed_to_string(line):
     str = str + '\t'
     for i in range(6, len(line)):
         str = str + line[i] + ' '
+
     return str[0:-1]
 
 
 def decompress(path):
     file = open(path, 'rb')
     fileData = zlib.decompress(file.read()).decode('utf-8')
-    return fileData # returns string
+    return fileData  # returns string
 
 
 def compress(fileData):
-    return zlib.compress(fileData.encode('utf-8')) # return bytes of compressed data
+    return zlib.compress(fileData.encode('utf-8'))  # return bytes of compressed data
 
-
+#parsing commit data
 def parse_commit(fileData):
     fileData = fileData.split('\n')
     commit = ''
@@ -68,7 +74,8 @@ def hash_string(fileData):
     return hashlib.sha1(fileData.encode('utf-8')).hexdigest()
 
 
-def make_new_file( commit, parents, author, committer, message):  # could replace with commit class
+def make_new_file(commit, parents, author, committer, message):  # could replace with commit class
+    #making string to hash
     fileData = ''
     fileData = fileData + "commit " + commit + "\n"
     for p in parents:
@@ -76,10 +83,11 @@ def make_new_file( commit, parents, author, committer, message):  # could replac
     fileData = fileData + "author " + author + "\n"
     fileData = fileData + "committer " + committer + "\n"
     fileData = fileData + "\n"
-    fileData = fileData + message +"\n"
+    fileData = fileData + message + "\n"
+
     hash = hash_string(fileData)
 
-    pathHash = homeDir + "object/" + hash[0:2] + "/" + hash[2:]
+    pathHash = homeDir + "objects/" + hash[0:2] + "/" + hash[2:]
 
     if not os.path.exists(os.path.dirname(pathHash)):
         os.makedirs(os.path.dirname(pathHash))
@@ -91,20 +99,28 @@ def make_new_file( commit, parents, author, committer, message):  # could replac
 
 
 def remove_file(hash):
-    path = homeDir + "object/" + hash[0:2] + "/" + hash[2:]
+    path = homeDir + "objects/" + hash[0:2] + "/" + hash[2:]
     os.remove(path)
 
 
 def modify_file_message(hash, newMessage):
-    path = homeDir + "object/" + hash[0:2] + "/" + hash[2:]
-    commit, parents, author, committer, message = parse_commit(path)
+    path = homeDir + "objects/" + hash[0:2] + "/" + hash[2:]
+    commit, parents, author, committer, message = parse_commit(decompress(path))
+
+    #changind length of data in commit message
+    delta = len(message) - len(newMessage)
+    print(commit[0:3])
+    newLength = int(commit[0:3]) - delta
+    commit = str(newLength) + commit[3:]
+
     commDict[hash] = make_new_file(commit, parents, author, committer, newMessage)
-    remove_file(hash)
+
+    return commDict[hash]
 
 
 def modify_file_parent(hash):
-    path = homeDir + "object/" + hash[0:2] + "/" + hash[3::]
-    commit, parents, author, committer, message = parse_commit(path)
+    path = homeDir + "objects/" + hash[0:2] + "/" + hash[2:]
+    commit, parents, author, committer, message = parse_commit(decompress(path))
     changed = False
     for i in range(len(parents)):
         if parents[i] in commDict:
@@ -113,20 +129,15 @@ def modify_file_parent(hash):
     if changed:
         commDict[hash] = make_new_file(commit, parents, author, committer, message)
 
+        return commDict[hash]
+    return hash
+
 
 logHEAD = homeDir + "logs/HEAD"
 logTemp = homeDir + "logs/temp"
-# inHash = sys.argv[1]
-# newMes = sys.argv[2]
-newMes = " "
-inHash = " "
-# print(inHash)
-#print(parse_commit(decompress("/home/kal/jet/.git/objects/1b/24e58ce94a37cea752c0a83e64c238d0d6b772")))
-#commit, parents, author, committer, message = (parse_commit(decompress("/home/kal/jet/.git/objects/1b/24e58ce94a37cea752c0a83e64c238d0d6b772")))
+inHash = sys.argv[1]
+newMes = sys.argv[2]
 
-#print(make_new_file(commit, parents, author, committer, message))
-
-#print(make_new_file(decompress("/home/kal/jet/.git/objects/1b/24e58ce94a37cea752c0a83e64c238d0d6b772")))
 
 log = open(logHEAD, 'r')
 newlog = open(logTemp, 'w')
@@ -137,7 +148,7 @@ Ended = False
 for _line in log.readlines():
 
     line = parse_line(_line)
-
+    print(line)
     if line[1] == inHash and not (line[1] in commDict):
         if newMes == line[-1][:-1]:
             print("nothing to modify")
@@ -146,10 +157,14 @@ for _line in log.readlines():
         line[1] = modify_file_message(inHash, newMes)
         line[-1] = newMes + '\n'
 
-    if line[0] in commDict:
-        line[1] = modify_file_parent(line[1])
-        line[0] = commDict[line[0]]
 
+    line[1] = modify_file_parent(line[1])
+
+    if line[0] in commDict:
+        line[0] = commDict[line[0]]
+    if line[1] in commDict:
+        line[1] = commDict[line[1]]
+    print(line)
     newlog.write(parsed_to_string(line))
 newlog.close()
 log.close()
@@ -157,6 +172,28 @@ log.close()
 if not Ended:
     os.remove(logHEAD)
     os.rename(logTemp, logHEAD)
+
+for hash in commDict:
+    remove_file(hash)
+    if not os.listdir(homeDir + "objects/" + hash[0:2]):
+        os.rmdir(homeDir + "objects/" + hash[0:2])
+# change other logs
+for log in os.listdir(homeDir + "logs/refs/heads/"):
+    f = open(homeDir + "logs/refs/heads/" + log, 'r')
+    g = open(homeDir + "logs/refs/heads/" + "temp", 'w')
+    for _line in f.readlines():
+
+        line = parse_line(_line)
+
+        if line[0] in commDict:
+            line[0] = commDict[line[0]]
+        if line[1] in commDict:
+            line[1] = commDict[line[1]]
+        g.write(parsed_to_string(line))
+    f.close()
+    g.close()
+    os.remove(homeDir + "logs/refs/heads/" + log)
+    os.rename(homeDir + "logs/refs/heads/" + "temp", homeDir + "logs/refs/heads/" + log)
 
 # change orig_head if needed
 with open(homeDir + "ORIG_HEAD") as f:
@@ -167,11 +204,13 @@ with open(homeDir + "ORIG_HEAD") as f:
         os.remove(homeDir + "ORIG_HEAD")
         os.rename(homeDir + "temp", homeDir + "ORIG_HEAD")
 
+# change refs
 for ref in os.listdir(homeDir + "refs/heads/"):
-   with open(homeDir + "refs/heads/" + ref) as f:
-       if f.read() in commDict:
-           g = open(homeDir + "refs/heads/" + "temp", 'w')
-           g.write(commDict[f.read()])
-           g.close()
-           os.remove(homeDir + "refs/heads/" + ref)
-           os.rename(homeDir + "refs/heads/" + "temp", homeDir + "refs/heads/" + ref)
+    with open(homeDir + "refs/heads/" + ref) as f:
+        h = f.read()[:-1]
+        if h in commDict:
+            g = open(homeDir + "refs/heads/" + "temp", 'w')
+            g.write(commDict[h])
+            g.close()
+            os.remove(homeDir + "refs/heads/" + ref)
+            os.rename(homeDir + "refs/heads/" + "temp", homeDir + "refs/heads/" + ref)
